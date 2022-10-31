52ed99fb543f54550071cdc48c0dccdc52e8bc68
nabila savitri <nabilasavitri1206@mhs.mdp.ac.id>
2022-10-31 15:12:19 +0700

    UPDATE

--- app/src/main/java/com/si5a/nabilasavitri/MainActivity.java
+++ app/src/main/java/com/si5a/nabilasavitri/MainActivity.java
@@ -3,12 +3,27 @@ package com.si5a.nabilasavitri;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.Spinner;
 
 public class MainActivity extends AppCompatActivity {
+    private EditText etnamalengkap, etnomorpendaftaran;
+    private Spinner spjalurpendaftaran;
+    private Button btndaftar;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        getSupportActionBar().setTitle("UTS Nabila Savitri");
+
+        btndaftar = findViewById(R.id.btn_daftar);
+        etnamalengkap = findViewById((R.id.et_namalengkap));
+        etnomorpendaftaran = findViewById(R.id.et_nomorpendaftaran);
+
     }
 }
